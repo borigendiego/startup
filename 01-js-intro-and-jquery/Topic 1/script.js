e55c8b45d13165b3bc3dd1de01443d1c0ac2a89a
borigendiego <borigendiego@gmail.com>
2015-03-12 11:52:04 -0300

    Correciones realizadas

--- 01-js-intro-and-jquery/Topic 1/script.js
+++ 01-js-intro-and-jquery/Topic 1/script.js
@@ -1,28 +1,9 @@
-/*5.	When the page has finished loading the section must fade in*/
-/*$(document).ready(function() {
-	$("p").hide(function() {
-		$("p").fadeIn(1000);
-	});
-});*/
 
-$( document ).ready(function() {
-  $( "#textbox" ).focus();
-});
 
 $(document).ready(function() {
+	$( "#textbox" ).focus();
 	$("section").show(function() {
 		$("p").fadeIn(1000);
 	});
 });
 
-/*function funcion()
-{
-var sectionColor = document.getElementById("section");
-if (document.getElementById('alias').value==""
-                 || document.getElementById('alias').value==undefined)
-                {
-                    sectionColor.style.backgroundColor = 'red';
-                }
-                else{
-                  sectionColor.style.backgroundColor = 'blue';
-                }*/
